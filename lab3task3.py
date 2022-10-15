243a3cc34cc6d0d1b6b1f0402a2af690de9aa7f5
Andrew <105111945+SadistVenturer@users.noreply.github.com>
2022-10-15 13:39:17 +0300

    Update lab3task3.py

--- lab3task3.py
+++ lab3task3.py
@@ -4,6 +4,7 @@ spend = 6000
 increase = 0.05
 month = 0  # количество месяцев, которое можно прожить
 x = (money_capital) + (salary) - (spend)
+month += 1
 while  True: 
     spend = (spend) * (1 + increase)
     x = (x) + (salary) - (spend)
