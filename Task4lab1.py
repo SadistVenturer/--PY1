b5068bba0d050fb71fbb27969c6c22bbb0b83829
Andrew <105111945+SadistVenturer@users.noreply.github.com>
2022-11-02 20:19:20 +0300

    Update Task4lab1.py

--- Task4lab1.py
+++ Task4lab1.py
@@ -1,12 +1,15 @@
 my_dict = {}
 def get_count_char(str_):
-    sym_count = 0
-    str_ = ' '.join(str_.lower().split())
+    str_ = str_.lower()
     for sym in str_:
-        if sym.isalpha() and my_dict.get(sym, True):
-            my_dict[sym] = str_.count(sym)
-        if  sym_count > 1:
-            return my_dict
-main_str = "''"
-""'"'
+        sym_count = str_.count(sym)
+        if sym.isalpha() and not my_dict.get(sym):
+            my_dict[sym] = sym_count
+    return my_dict
+
+main_str = """
+Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
 print(get_count_char(main_str))
