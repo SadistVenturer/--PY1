c1684bb05a2521eacf6b53134423d0c3e499a19f
Andrew <105111945+SadistVenturer@users.noreply.github.com>
2022-11-02 21:17:20 +0300

    Update Task4lab1.py

--- Task4lab1.py
+++ Task4lab1.py
@@ -6,10 +6,15 @@ def get_count_char(str_):
         if sym.isalpha() and not my_dict.get(sym):
             my_dict[sym] = sym_count
     return my_dict
-
 main_str = """
 Данное предложение будет разбиваться на отдельные слова. 
     В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
 print(get_count_char(main_str))
+def get_procent_char(my_dict_):
+    counter = sum(my_dict_.values())
+    for sym in my_dict_:
+        my_dict_[sym] = round(my_dict_[sym] * 100 / counter, 2)
+    return my_dict_
+print(get_procent_char(my_dict))
